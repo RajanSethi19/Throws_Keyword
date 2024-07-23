86d55fb14043b21c0564f5acabae3d806eeac02d
Rajan <rajansethi19@gmail.com>
2024-07-23 15:02:20 +0530

    Throws keyword

---
+++ Exceptions.java
@@ -0,0 +1,17 @@
+package throw_keyeord;
+
+public class Exceptions {
+
+	public static void main(String[] args) throws NullPointerException, InterruptedException
+	
+	
+	{
+		System.out.println("Rajan");
+		Thread.sleep(3000);
+		System.out.println("sethi");
+		throw new NullPointerException("Sorry the cell is empty");
+		
+		
+	}
+
+}
